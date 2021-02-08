eaee17167423c2888288c32b372eb84494aa5d6b
dm1tro69 <dimolg22@gmail.com>
2021-02-08 23:05:52 +0200

    add Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import styled from "styled-components";
+import {Home,
+    SingleProduct,
+    Checkout,
+    Error,
+    Cart,
+    About,
+    Products,
+    Private} from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return(
+      <Router>
+          <Navbar/>
+          <Sidebar/>
+          <Switch>
+             <Route exact path={'/'} component={Home}/>
+             <Route  path={'/about'} component={About}/>
+             <Route  path={'/cart'} component={Cart}/>
+             <Route  path={'/products'} component={Products}/>
+             <Route  path={'/products/:id'} component={SingleProduct}/>
+             <Route  path={'/checkout'} component={Checkout}/>
+             <Route path={'*'} component={Error}/>
+          </Switch>
+          <Footer/>
+      </Router>
+      )
+
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './HomePage.js'
+import About from './AboutPage.js'
+import Auth from './AuthWrapper.js'
+import Cart from './CartPage.js'
+import Checkout from './CheckoutPage.js'
+import Error from './ErrorPage.js'
+import Private from './PrivateRoute.js'
+import Products from './ProductsPage.js'
+import SingleProduct from './SingleProductPage.js'
+
+export {Home, About, Auth, Cart, Checkout, Error, Private, Products, SingleProduct}
